5c36a49f5e7011f7f466985be44a0e68d87400ae
rer145 <ron.richardson@gmail.com>
2015-04-16 11:11:03 -0400

    Added results management page

---
+++ RonsHouse.FantasyGolf.Web/admin/results.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="results.aspx.cs" Inherits="RonsHouse.FantasyGolf.Web.Admin.AdminResultsPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+		<p>Tournament: <asp:DropDownList ID="tournament_list" runat="server" DataTextField="Name" DataValueField="Id" AutoPostBack="true" OnSelectedIndexChanged="OnSelectTournament" /></p>
+		<p>Golfer: <asp:DropDownList ID="golfer_list" runat="server" DataTextField="Name" DataValueField="Id" /></p>
+		<p>Place: <asp:TextBox ID="place_textbox" runat="server" Width="50" /></p>
+		<p>Winnings: $<asp:TextBox ID="winnings_textbox" runat="server" Width="100" /></p>
+		<p>Cut: <asp:CheckBox ID="cut_checkbox" runat="server" /></p>
+		<p>Withdrew: <asp:CheckBox ID="wd_checkbox" runat="server" /></p>
+		<p>Disqualified: <asp:CheckBox ID="dq_checkbox" runat="server" /></p>
+		<p>Tied: <asp:CheckBox ID="tied_checkbox" runat="server" /></p>
+		<p>Playoff: <asp:CheckBox ID="playoff_checkbox" runat="server" /></p>
+
+		<p><asp:Button id="submit_button" runat="server" Text="Save Results" OnClick="OnSaveResults" /></p>
+
+		<p><asp:Label ID="message_label" runat="server" /></p>
+
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+
+		<table border="0" width="100%" cellpadding="8" cellspacing="0">
+		<tr>
+			<td width="50%" valign="top">
+				<h2>Tournament Results</h2>
+				<asp:GridView ID="results_grid" runat="server" AutoGenerateColumns="false" CellPadding="4">
+					<Columns>
+						<asp:BoundField HeaderText="First Name" DataField="FirstName" />
+						<asp:BoundField HeaderText="Last Name" DataField="LastName" />
+						<asp:BoundField HeaderText="Place" DataField="Place" />
+						<asp:BoundField HeaderText="Winnings" DataField="Winnings" DataFormatString="{0:C2}" />
+					</Columns>
+				</asp:GridView>
+			</td>
+			<td width="50%" valign="top">
+				<h2>Current Standings</h2>
+				<asp:GridView ID="standings_grid" runat="server" AutoGenerateColumns="false" CellPadding="4">
+					<Columns>
+						<asp:BoundField HeaderText="First Name" DataField="FirstName" />
+						<asp:BoundField HeaderText="Last Name" DataField="LastName" />
+						<asp:BoundField HeaderText="Winnings" DataField="Winnings" DataFormatString="{0:C2}" />
+					</Columns>
+				</asp:GridView>
+			</td>
+		</tr>
+		</table>
+
+    </form>
+</body>
+</html>
---
+++ RonsHouse.FantasyGolf.Web/admin/results.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using RonsHouse.FantasyGolf.Model;
+
+using Dapper;
+
+namespace RonsHouse.FantasyGolf.Web.Admin
+{
+	public partial class AdminResultsPage : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!Page.IsPostBack)
+			{
+				SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ConnectionString);
+				connection.Open();
+				
+				var tournaments = connection.Query<Tournament>("select * from Tournament order by BeginsOn");
+				tournament_list.DataSource = tournaments;
+				tournament_list.DataBind();
+				tournament_list.Items.Insert(0, "");
+				tournament_list.SelectedIndex = 0;
+
+				connection.Close();
+
+				message_label.Visible = false;
+			}
+		}
+
+		protected void OnSelectTournament(object sender, EventArgs e)
+		{
+			SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ConnectionString);
+			connection.Open();
+
+			if (!String.IsNullOrEmpty(tournament_list.SelectedValue))
+			{
+				var golfers = connection.Query<Golfer>("select distinct g.*, g.LastName + ', ' + g.FirstName as Name from Golfer g inner join UserPick up on up.GolferId = g.Id where up.TournamentId = " + tournament_list.SelectedValue + " order by LastName");
+				golfer_list.DataSource = golfers;
+				golfer_list.DataBind();
+				golfer_list.Items.Insert(0, "");
+				golfer_list.SelectedIndex = 0;
+			}
+
+			connection.Close();
+
+			BindGrids();
+		}
+
+		protected void OnSaveResults(object sender, EventArgs e)
+		{
+			//save values
+			SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ConnectionString);
+			connection.Open();
+
+			connection.Query("TournamentResult_Set", new
+			{
+				TournamentId = tournament_list.SelectedValue,
+				GolferId = golfer_list.SelectedValue,
+				Place = place_textbox.Text,
+				Winnings = winnings_textbox.Text,
+				IsCut = cut_checkbox.Checked,
+				IsTied = tied_checkbox.Checked,
+				IsWithdrawn = wd_checkbox.Checked,
+				IsDisqualified = dq_checkbox.Checked,
+				IsPlayoff = playoff_checkbox.Checked
+			}, commandType: CommandType.StoredProcedure);
+
+			connection.Close();
+
+			//Response.Write("tournament: " + tournament_list.SelectedValue + "<br />");
+			//Response.Write("golfer: " + golfer_list.SelectedValue + "<br />");
+			//Response.Write("place: " + place_textbox.Text + "<br />");
+			//Response.Write("winnings: " + winnings_textbox.Text + "<br />");
+			//Response.Write("cut: " + cut_checkbox.Checked + "<br />");
+			//Response.Write("tied: " + tied_checkbox.Checked + "<br />");
+			//Response.Write("wd: " + wd_checkbox.Checked + "<br />");
+			//Response.Write("dq: " + dq_checkbox.Checked + "<br />");
+			//Response.Write("playoff: " + playoff_checkbox.Checked + "<br />");
+			
+			
+			message_label.Visible = true;
+			message_label.Text = "Result was saved";
+
+			//reset values
+			golfer_list.SelectedIndex = 0;
+			place_textbox.Text = "";
+			winnings_textbox.Text = "";
+			cut_checkbox.Checked = false;
+			wd_checkbox.Checked = false;
+			tied_checkbox.Checked = false;
+			playoff_checkbox.Checked = false;
+			dq_checkbox.Checked = false;
+
+			BindGrids();
+		}
+
+		protected void BindGrids()
+		{
+			using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ConnectionString))
+			{
+				connection.Open();
+
+				using (SqlCommand cmd = new SqlCommand("User_GetStandings", connection))
+				{
+					cmd.CommandType = CommandType.StoredProcedure;
+
+					IDataReader data = cmd.ExecuteReader();
+					standings_grid.DataSource = data;
+					standings_grid.DataBind();
+					data.Close();
+				}
+
+				using (SqlCommand cmd = new SqlCommand("Tournament_GetResults", connection))
+				{
+					cmd.CommandType = CommandType.StoredProcedure;
+					cmd.Parameters.Add(new SqlParameter("TournamentId", tournament_list.SelectedValue));
+
+					IDataReader data = cmd.ExecuteReader();
+					results_grid.DataSource = data;
+					results_grid.DataBind();
+					data.Close();
+				}
+
+				connection.Close();
+			}
+		}
+	}
+}
