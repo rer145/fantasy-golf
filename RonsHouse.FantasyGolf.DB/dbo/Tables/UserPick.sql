8b7004cc84c19abf19128cbafd1750bf44fbbebd
rer145 <ron.richardson@gmail.com>
2015-04-30 14:45:11 -0400

    Updated database project with new Identity tables.  Also fixed primary key on UserPick so it will work with EF.

--- RonsHouse.FantasyGolf.DB/dbo/Tables/UserPick.sql
+++ RonsHouse.FantasyGolf.DB/dbo/Tables/UserPick.sql
@@ -3,9 +3,11 @@
     [TournamentId] INT      NOT NULL,
     [GolferId]     INT      NOT NULL,
     [CreatedOn]    DATETIME CONSTRAINT [DF_UserPick_CreatedOn] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [PK_UserPick] PRIMARY KEY CLUSTERED ([UserId] ASC, [TournamentId] ASC, [GolferId] ASC),
+    CONSTRAINT [PK_UserPick_1] PRIMARY KEY CLUSTERED ([UserId] ASC, [TournamentId] ASC),
     CONSTRAINT [FK_UserPick_Golfer] FOREIGN KEY ([GolferId]) REFERENCES [dbo].[Golfer] ([Id]),
     CONSTRAINT [FK_UserPick_Tournament] FOREIGN KEY ([TournamentId]) REFERENCES [dbo].[Tournament] ([Id]),
     CONSTRAINT [FK_UserPick_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
 );
+
+
 
